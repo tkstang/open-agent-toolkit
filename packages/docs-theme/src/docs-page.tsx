87a43fa938ef912132954890e6a3d27a18c62a54
Thomas Stang <thomas.stang@voxmedia.com>
2026-03-08 20:24:36 -0500

    feat(p01-t09): implement DocsPage component

--- packages/docs-theme/src/docs-page.tsx
+++ packages/docs-theme/src/docs-page.tsx
@@ -1,9 +1,16 @@
+import type { TableOfContents } from 'fumadocs-core/server';
+import { DocsBody, DocsPage as FumaDocsPage } from 'fumadocs-ui/page';
 import type { ReactNode } from 'react';
 
 export interface DocsPageProps {
-  children?: ReactNode;
+  toc: TableOfContents;
+  children: ReactNode;
 }
 
-export function DocsPage({ children }: DocsPageProps) {
-  return <>{children}</>;
+export function DocsPage({ toc, children }: DocsPageProps) {
+  return (
+    <FumaDocsPage toc={toc}>
+      <DocsBody>{children}</DocsBody>
+    </FumaDocsPage>
+  );
 }
