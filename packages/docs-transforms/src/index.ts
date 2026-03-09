1e60197512aa07f08003618dc6cb6a2d1346d7e2
Thomas Stang <thomas.stang@voxmedia.com>
2026-03-08 20:15:23 -0500

    feat(p01-t03): implement remarkTabs transform

--- packages/docs-transforms/src/index.ts
+++ packages/docs-transforms/src/index.ts
@@ -1,3 +1,7 @@
+import type { Root } from 'mdast';
 import type { Plugin } from 'unified';
+import { remarkTabs } from './remark-tabs.js';
 
-export const defaultTransforms: Plugin[] = [];
+export { remarkTabs } from './remark-tabs.js';
+
+export const defaultTransforms: Plugin<[], Root>[] = [remarkTabs];
